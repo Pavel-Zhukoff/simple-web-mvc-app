dcc4c819f0044280b4d172d6e4264ff879e0315a
Pavel-Zhukoff <pavel.zhuko.ff@yandex.ru>
2020-01-08 22:32:03 +0300

    some fixes

--- src/main/java/ru/pavel_zhukoff/annotations/RequestParam.java
+++ src/main/java/ru/pavel_zhukoff/annotations/RequestParam.java
@@ -2,6 +2,13 @@ package ru.pavel_zhukoff.annotations;
 
 import ru.pavel_zhukoff.enums.RequestType;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface RequestParam {
     String uri() default "";
     RequestType requsetType() default RequestType.GET;
